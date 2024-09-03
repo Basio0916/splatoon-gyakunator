c23eb82147f64e9ede2b2fc2ffb7978d2e85f5ec
Makoto Ishibashi <arsoncase@gmail.com>
2024-09-04 08:16:11 +0900

    GameStartModal作成

---
+++ src/components/GameStartModal.tsx
@@ -0,0 +1,50 @@
+import { Button, Card, Modal } from "@mui/material";
+import { AnswerHistory } from "./AnswerHistory";
+import { Answer } from "../types/Answer";
+import { FC } from "react";
+
+type Props = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  answerHistory: Array<Answer>;
+};
+
+export const GameStartModal: FC<Props> = (props) => {
+  const { open, setOpen, answerHistory } = props;
+  const handleClose = (_event: {}, reason: string) => {
+    if (reason !== "backdropClick") {
+      setOpen(false);
+    }
+  };
+  const onClick = (_event: React.MouseEvent<HTMLButtonElement>) => {
+    setOpen(false);
+  };
+  return (
+    <Modal open={open} onClose={handleClose} sx={{}}>
+      <Card
+        sx={{
+          padding: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          outline: "none",
+        }}
+      >
+        <div style={{ minWidth: "400px", width: "100%" }}>
+          <AnswerHistory answerHistory={answerHistory} />
+        </div>
+        <Button
+          variant="contained"
+          sx={{ width: "150px", fontSize: "18px", marginTop: "10px" }}
+          onClick={onClick}
+        >
+          スタート
+        </Button>
+      </Card>
+    </Modal>
+  );
+};
