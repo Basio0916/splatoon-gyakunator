861cb5b883b77b48ed5e7165aaabf176c7142db7
Makoto Ishibashi <arsoncase@gmail.com>
2024-09-14 22:21:37 +0900

    回答時の入力性を向上

--- src/components/AnswerSubmissionModal.tsx
+++ src/components/AnswerSubmissionModal.tsx
@@ -47,6 +47,37 @@ export const AnswerSubmissionModal: FC<Props> = (props) => {
     onClose(weaponSelect, result);
   };
 
+  const toKatakana = (input: string): string => {
+    return input.replace(/[\u3041-\u3096]/g, (char) =>
+      String.fromCharCode(char.charCodeAt(0) + 0x60)
+    );
+  };
+
+  const toHiragana = (input: string): string => {
+    return input.replace(/[\u30A1-\u30F6]/g, (char) =>
+      String.fromCharCode(char.charCodeAt(0) - 0x60)
+    );
+  };
+
+  const filterOptions = (
+    options: string[],
+    { inputValue }: { inputValue: string }
+  ) => {
+    const lowerCaseInput = inputValue.toLowerCase();
+    const katakanaInput = toKatakana(lowerCaseInput);
+    const hiraganaInput = toHiragana(lowerCaseInput);
+
+    return options.filter((option) => {
+      const lowerCaseOption = option.toLowerCase();
+
+      return (
+        lowerCaseOption.includes(lowerCaseInput) ||
+        lowerCaseOption.includes(katakanaInput) ||
+        lowerCaseOption.includes(hiraganaInput)
+      );
+    });
+  };
+
   return (
     <Modal open={open} onClose={handleClose} sx={{}}>
       <Card
@@ -74,6 +105,7 @@ export const AnswerSubmissionModal: FC<Props> = (props) => {
           getOptionLabel={(option) => option}
           value={weaponSelect}
           onChange={handleWeaponChange}
+          filterOptions={filterOptions}
           renderInput={(params) => (
             <TextField {...params} label="回答" variant="outlined" />
           )}
