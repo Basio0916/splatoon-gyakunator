ec1ce4f4d9b7391783dbcc748cf765a826b0bd79
Makoto Ishibashi <arsoncase@gmail.com>
2024-10-07 00:00:32 +0900

    SpecialWeaponを作成

---
+++ backend/app/domain/model/SpecialWeapon.scala
@@ -0,0 +1,10 @@
+case class SpecialWeapon(
+    val name: String,
+    val hasZRButtonAction: Answer,
+    val hasRButtonAction: Answer,
+    val canOneShot: Answer,
+    val dealsContinuousDamage: Answer,
+    val isAttackType: Answer,
+    val canSuperJump: Answer,
+    val jumpsToUsePoint: Answer,
+)
