2c12797c60b99be75dbce864b0bfa4fe981f19e8
Makoto Ishibashi <arsoncase@gmail.com>
2024-10-07 00:00:41 +0900

    SuSubWeaponを作成

---
+++ backend/app/domain/model/SubWeapon.scala
@@ -0,0 +1,9 @@
+case class SubWeapon(
+    val name: String,
+    val inkConsumption: Int,
+    val damage: (Option[Double], Option[Double], Option[Double]),
+    val isAttackType: Answer,
+    val canSet: Answer,
+    val canInk: Answer,
+    val canSensor: Answer
+)
