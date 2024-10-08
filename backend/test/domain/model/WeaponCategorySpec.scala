764c8aab06f243539c91c50fa4361f961c82ed86
Makoto Ishibashi <arsoncase@gmail.com>
2024-10-08 17:24:13 +0900

    WeaponCategoryのテストを追加

---
+++ backend/test/domain/model/WeaponCategorySpec.scala
@@ -0,0 +1,57 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.matchers.should.Matchers
+
+class WeaponCategorySpec extends AnyFlatSpec with TableDrivenPropertyChecks with Matchers {
+    "WeaponCategory" should "override toString to display its class name" in {
+        val examples = Table(
+            ("WeaponCategory", "expectedResult"),
+            (Shooter, "シューター"),
+            (Roller, "ローラー"),
+            (Charger, "チャージャー"),
+            (Slosher, "スロッシャー"),
+            (Blaster, "ブラスター"),
+            (Splatling, "スピナー"),
+            (Brella, "シェルター"),
+            (Dualies, "マニューバー"),
+            (Brush, "フデ"),
+            (Stringers, "ストリンガー"),
+            (Splatanas, "ワイパー")
+        )
+        forAll(examples) { (weaponCategory, expectedResult) =>
+            weaponCategory.toString should equal(expectedResult)
+        }
+    }
+
+    "from" should "return WeaponCategory from a string" in {
+        val examples = Table(
+            ("value", "expectedResult"),
+            ("シューター", Shooter),
+            ("ローラー", Roller),
+            ("チャージャー", Charger),
+            ("スロッシャー", Slosher),
+            ("ブラスター", Blaster),
+            ("スピナー", Splatling),
+            ("シェルター", Brella),
+            ("マニューバー", Dualies),
+            ("フデ", Brush),
+            ("ストリンガー", Stringers),
+            ("ワイパー", Splatanas)
+        )
+        forAll(examples) { (value, expectedResult) =>
+            WeaponCategory.from(value) should equal(expectedResult)
+        }
+    }
+
+    it should "throw IllegalArgumentException for invalid string" in {
+        val examples = Table(
+            "value",
+            "invalid"
+        )
+        forAll(examples) { value =>
+            an[IllegalArgumentException] should be thrownBy {
+                WeaponCategory.from(value)
+            }
+        }
+    }
+}
