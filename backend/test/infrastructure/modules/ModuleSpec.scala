706eecd32c1349fb42af7131e2c59b66cabf24bb
Makoto Ishibashi <arsoncase@gmail.com>
2024-10-13 23:38:21 +0900

    GuiceのDIを確認するテストを追加

---
+++ backend/test/infrastructure/modules/ModuleSpec.scala
@@ -0,0 +1,22 @@
+package infrastructure.modules
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.matchers.should.Matchers
+import com.google.inject.Guice
+import domain.repositories.WeaponRepository
+
+class ModuleSpec extends AnyFlatSpec with TableDrivenPropertyChecks with Matchers{
+ 
+    val injector = Guice.createInjector(new Module)
+    val weaponRepository = injector.getInstance(classOf[WeaponRepository])
+
+    "WeaponRepository" should "return the correct weapon" in {
+        val weaponRepository = injector.getInstance(classOf[WeaponRepository])
+        val weapon = weaponRepository.findWeaponByName("わかばシューター")
+        weapon.get.name should equal("わかばシューター")
+        weapon.get.mainWeapon.name should equal("わかばシューター")
+        weapon.get.subWeapon.name should equal("スプラッシュボム")
+        weapon.get.specialWeapon.name should equal("グレートバリア")
+    }
+}
